57211d980cae6bbaf64ac4f17a7b3e3c0c3c9b09
MacAdmin <macadmin@MacAdmins-Mac-Pro.local>
2015-02-26 09:58:20 -0800

    Initial Commit

---
+++ KeyValueCodingSample/KVCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KVCAppDelegate.h
+//  KeyValueCodingSample
+//
+//  Created by MacAdmin on 2/26/15.
+//  Copyright (c) 2015 ___TGSoft___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KVCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KeyValueCodingSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KeyValueCodingSample
+//
+//  Created by MacAdmin on 2/26/15.
+//  Copyright (c) 2015 ___TGSoft___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KVCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KVCAppDelegate class]));
+    }
+}
